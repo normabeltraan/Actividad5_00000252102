1132526b6371a6efc30d294b3ad3cb9f6bcdfa35
normabeltraan <norma07beltran@hotmail.com>
2026-02-25 19:57:05 -0700

    Actividad 5 terminada.

---
+++ app/src/main/java/mx/edu/itson/potros/practica6/MovieDetailActivity.kt
@@ -0,0 +1,34 @@
+package mx.edu.itson.potros.practica6
+
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MovieDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_movie_detail)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val iv_pelicula_image: ImageView = findViewById(R.id.iv_pelicula_imagen)
+        val tv_nombre_pelicula: TextView = findViewById(R.id.tv_nombre_pelicula)
+        val tv_pelicula_desc: TextView = findViewById(R.id.tv_pelicula_desc)
+
+        val bundle = intent.extras
+        if (bundle != null) {
+            iv_pelicula_image.setImageResource(bundle.getInt("header"))
+            tv_nombre_pelicula.setText(bundle.getString("titulo"))
+            tv_pelicula_desc.setText(bundle.getString("sinopsis"))
+        }
+    }
+}
