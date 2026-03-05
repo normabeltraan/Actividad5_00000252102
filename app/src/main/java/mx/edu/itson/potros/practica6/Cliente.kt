dd4384c092b89a2c2815d604eb556907b1c72765
normabeltraan <norma07beltran@hotmail.com>
2026-03-04 19:56:20 -0700

    Práctica 6: Diseño UI Tickets

---
+++ app/src/main/java/mx/edu/itson/potros/practica6/Cliente.kt
@@ -0,0 +1,4 @@
+package mx.edu.itson.potros.practica6
+
+data class Cliente (var nombre:String, var tipoPago:String, var asiento:Int){
+}
--- app/src/main/java/mx/edu/itson/potros/practica6/MovieDetailActivity.kt
+++ app/src/main/java/mx/edu/itson/potros/practica6/MovieDetailActivity.kt
@@ -1,6 +1,8 @@
 package mx.edu.itson.potros.practica6
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -9,26 +11,64 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MovieDetailActivity : AppCompatActivity() {
+
+    lateinit var seatLeft: TextView
+    var posMovie: Int = -1
+    var tipo: String? = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_movie_detail)
+
+        val iv_pelicula_image: ImageView = findViewById(R.id.iv_pelicula_imagen)
+        val tv_nombre_pelicula: TextView = findViewById(R.id.tv_nombre_pelicula)
+        val tv_pelicula_desc: TextView = findViewById(R.id.tv_pelicula_desc)
+        val btn_buy_tickets: Button = findViewById(R.id.buyTickets)
+        seatLeft = findViewById(R.id.seatLeft)
+
+        val bundle = intent.extras
+        if (bundle != null) {
+            iv_pelicula_image.setImageResource(bundle.getInt("header"))
+            tv_nombre_pelicula.text = bundle.getString("titulo")
+            tv_pelicula_desc.text = bundle.getString("sinopsis")
+
+            posMovie = bundle.getInt("pos", -1)
+            tipo = bundle.getString("tipo")
+
+            actualizarAsientos()
+        }
+
+        btn_buy_tickets.setOnClickListener {
+            val intent = Intent(this, SeatSelectionActivity::class.java)
+            intent.putExtra("name", tv_nombre_pelicula.text.toString())
+            intent.putExtra("id", posMovie)
+            intent.putExtra("tipo", tipo)
+            startActivity(intent)
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
 
-        val iv_pelicula_image: ImageView = findViewById(R.id.iv_pelicula_imagen)
-        val tv_nombre_pelicula: TextView = findViewById(R.id.tv_nombre_pelicula)
-        val tv_pelicula_desc: TextView = findViewById(R.id.tv_pelicula_desc)
+    fun actualizarAsientos() {
+        if (posMovie != -1) {
+            val pelicula = if (tipo == "peli") {
+                CatalogActivity.peliculas[posMovie]
+            } else {
+                CatalogActivity.series[posMovie]
+            }
 
-        val bundle = intent.extras
-        if (bundle != null) {
-            iv_pelicula_image.setImageResource(bundle.getInt("header"))
-            tv_nombre_pelicula.setText(bundle.getString("titulo"))
-            tv_pelicula_desc.setText(bundle.getString("sinopsis"))
+            val disponibles = 20 - pelicula.seats.size
+            seatLeft.text = "$disponibles seats available"
         }
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        actualizarAsientos()
     }
 }
